e9e8271986b03a4edb52ca75a87820330e86a730
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-07-29 11:46:37 +0300

    add

--- js/sidebar-toggle.js
+++ js/sidebar-toggle.js
@@ -1,6 +1,25 @@
+
 const toggle = document.querySelector('.js-sidebar-toggle');
 const menu = document.querySelector('.sidebar__links-list');
 
+function setMenuState() {
+    const isDesktop = window.innerWidth > 1024;
+
+    if (isDesktop) {
+        toggle.classList.add('is-open');
+        menu.style.height = 'auto';
+    } else {
+        toggle.classList.remove('is-open');
+        menu.style.height = '0px';
+    }
+}
+
+// Run once on load
+window.addEventListener('DOMContentLoaded', setMenuState);
+// Re-run on resize
+window.addEventListener('resize', setMenuState);
+
+// Toggle interaction
 toggle.addEventListener('click', () => {
     const isOpen = toggle.classList.toggle('is-open');
 
